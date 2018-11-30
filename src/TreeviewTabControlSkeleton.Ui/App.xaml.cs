255ff1a540e2f270a483788daf4cdd76477dc76c
Patrick Gehrke <39940651+patrickgehrke@users.noreply.github.com>
2018-11-30 17:56:03 +0100

    ADD: caliburn micro startup code

--- src/TreeviewTabControlSkeleton.Ui/App.xaml.cs
+++ src/TreeviewTabControlSkeleton.Ui/App.xaml.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
+using TreeviewTabControlSkeleton.Ui.Common;
 
 namespace TreeviewTabControlSkeleton.Ui
 {
@@ -13,5 +10,32 @@ namespace TreeviewTabControlSkeleton.Ui
     /// </summary>
     public partial class App : Application
     {
+        private static Mutex mutex;
+
+        public App()
+        {
+            this.Resources.Add("Bootstrapper", new Bootstrapper());
+
+            mutex = new Mutex(true, "TreeviewTabControlSkeleton" + Environment.UserName, out var mutexNew);
+
+            if (!mutexNew)
+            {
+                mutex.Dispose();
+                throw new Exception("Mutex TreeviewTabControlSkeleton");
+
+            }
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if (mutex != null)
+            {
+                mutex.ReleaseMutex();
+                mutex.Dispose();
+                mutex = null;
+            }
+
+            base.OnExit(e);
+        }
     }
 }
---
+++ src/TreeviewTabControlSkeleton.Ui/Common/Bootstrapper.cs
@@ -0,0 +1,57 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Windows;
+using TreeviewTabControlSkeleton.Ui.ViewModels;
+
+namespace TreeviewTabControlSkeleton.Ui.Common
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer container;
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            container = new SimpleContainer();
+
+            container.Singleton<IWindowManager, WindowManager>();
+
+            container.PerRequest<ShellViewModel>();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
+        }
+
+        protected override IEnumerable<Assembly> SelectAssemblies()
+        {
+            return new[]
+            {
+                Assembly.GetExecutingAssembly(),
+                Assembly.Load("TreeviewTabControlSkeleton.WpfInfrastructure")
+            };
+        }
+    }
+}
