04a025db2f5ee178231c05ba8fdd0f466343936c
Patrick Gehrke <39940651+patrickgehrke@users.noreply.github.com>
2018-12-08 16:05:32 +0100

    REF: TreeNode property naming

--- src/TreeviewTabControlSkeleton.Ui/ViewModels/ShellViewModel.cs
+++ src/TreeviewTabControlSkeleton.Ui/ViewModels/ShellViewModel.cs
@@ -14,11 +14,10 @@ namespace TreeviewTabControlSkeleton.Ui.ViewModels
             this.Version = "1.0 DEBUG";
 
             /*will be replaced - dynamic treeview from database */
-            this.TreeviewItems = new ObservableCollection<TreeNodeViewModel>();
-            this.TreeviewItems.Add(new TreeNodeViewModel("Dashboard", LogoResources.Database, true));
-            this.TreeviewItems[0].Childs = new ObservableCollection<TreeNodeViewModel>();
-            this.TreeviewItems[0].Childs.Add(new TreeNodeViewModel("first child", LogoResources.PlayerProfile, false));
-            
+            this.TreeNodes = new ObservableCollection<TreeNodeViewModel>();
+            this.TreeNodes.Add(new TreeNodeViewModel("Dashboard", LogoResources.Database, true));
+            this.TreeNodes[0].Childs = new ObservableCollection<TreeNodeViewModel>();
+            this.TreeNodes[0].Childs.Add(new TreeNodeViewModel("first child", LogoResources.PlayerProfile, false));
         }
 
         public PathGeometry ApplicationLogo = LogoResources.Github;
@@ -27,6 +26,6 @@ namespace TreeviewTabControlSkeleton.Ui.ViewModels
 
         public string Version { get; set; }
 
-        public ObservableCollection<TreeNodeViewModel> TreeviewItems { get; set; }
+        public ObservableCollection<TreeNodeViewModel> TreeNodes { get; set; }
     }
 }
